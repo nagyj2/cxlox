3b546a084bebd4f5c0a267319737b6cde60dc6b7
Jason Nagy <jaysun_n@hotmail.com>
2022-04-20 20:24:23 -0400

    Disabled all debug defines

--- inc/common.h
+++ inc/common.h
@@ -6,15 +6,16 @@
 #include <stdint.h> // uint8_t
 
 // Enables chunk dumping when the compiler finishes with the chunk.
-#define DEBUG_PRINT_CODE
+// #define DEBUG_PRINT_CODE
 
 // Debug define to allow easier viewing of VM internals, including stack.
-#define DEBUG_TRACE_EXECUTION
+// #define DEBUG_TRACE_EXECUTION
 
 // Stress test mode for the garbage collector. Makes the GC run whenever it can.
 // #define DEBUG_STRESS_GC
+
 // Perform debug logging of the GC operations.
-#define DEBUG_LOG_GC
+// #define DEBUG_LOG_GC
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
